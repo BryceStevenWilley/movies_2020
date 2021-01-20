cf6cd55af045f5e36d8b9aee22ef9ec49583bbdf
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-01-20 00:18:33 -0500

    Added Mako template, got everything in it

--- src/movie_viz.py
+++ src/movie_viz.py
@@ -9,205 +9,219 @@ from pathlib import Path
 import collections
 import itertools
 import networkx as nx
+from mako.template import Template
 
 import numpy as np
 import pandas as pd
-import geopandas 
+import geopandas
 import imdb
 import plotly.graph_objects as go
 import plotly.express as px
 import geopy
 from geopy.extra.rate_limiter import RateLimiter
 
+
+mov_cache_f = Path(__file__).resolve().parent.joinpath('imdb_cache.p')
+imdb_cache = pickle.load(mov_cache_f.open('rb')) if mov_cache_f.exists() else dict()
+
 ia = imdb.IMDb()
-
-imdb_cache_path = 'imdb_cache.p'
-
-imdb_cache = dict()
-if Path(imdb_cache_path).exists():
-  imdb_cache = pickle.load(open(imdb_cache_path, 'rb'))
-
 def imdb_search(mov_str):
-  if mov_str not in imdb_cache:
-    mov_id = ia.search_movie(mov_str)[0].movieID # assuming the 1st is the best match.
-    full_mov = ia.get_movie(mov_id)
-    # https://imdbpy.readthedocs.io/en/latest/usage/data-interface.html
-    ia.update(full_mov, info=['locations', 'release dates'])
-    imdb_cache[mov_str] = full_mov
-  return imdb_cache[mov_str]
+    if mov_str not in imdb_cache:
+        mov_id = ia.search_movie(mov_str)[0].movieID  # assuming the 1st is the best match.
+        full_mov = ia.get_movie(mov_id)
+        # https://imdbpy.readthedocs.io/en/latest/usage/data-interface.html
+        ia.update(full_mov, info=['locations', 'release dates'])
+        imdb_cache[mov_str] = full_mov
+    return imdb_cache[mov_str]
 
 
 def get_movie_info(input_file):
-  df = pd.read_csv(input_file)
-  df['Date Watched'] = pd.to_datetime(df['Date Watched'])
-  full_movies = df['Movie Name'].apply(lambda x: imdb_search(x))
-  pickle.dump(imdb_cache, open(imdb_cache_path, 'wb'))
-  df['index'] = df.index # So you can index into the movie list, which can't be added to the df
-  return df, full_movies
+    df = pd.read_csv(input_file)
+    df['Date Watched'] = pd.to_datetime(df['Date Watched'])
+    full_movies = df['Movie Name'].apply(lambda x: imdb_search(x))
+    pickle.dump(imdb_cache, mov_cache_f.open('wb'))
+    df['index'] = df.index  # So you can index into the movie list, which can't be added to the df
+    return df, full_movies
 
-# some cool sns stuff, not necessary
-# sns.jointplot(x='Date Watched', y='Year', data=df, kind='scatter', color='#4cb391'); plt.show();
 
-def combo_timeline(df, output_f):
-  # https://plotly.com/python/marginal-plots/
-  fig = px.scatter(df, x="Date Watched", y="Year", marginal_x='histogram', marginal_y='histogram', title='Test')
-  output_f.write(fig.to_html(full_html=True, include_plotlyjs='cdn'))
-  return fig
+def combo_timeline(df):
+    df_mod = df.replace(star_vals)
+    df_mod['Good'] = (df_mod['Bryce Rating'] + df_mod['Kathy Rating'] / 2) > 3
+    # https://plotly.com/python/marginal-plots/
+    fig = px.scatter(df_mod, x="Date Watched", y="Year", labels='Movie Name',
+                     color='Good', hover_data=['Movie Name'],
+                     marginal_x='histogram',
+                     marginal_y='histogram', title='Date Watched vs Year Made')
+    fig.update_layout(width=1000, height=1000) 
+    return fig.to_html(full_html=False, include_plotlyjs='cdn')
 
 
 star_vals = {
-"1/2": 0.5,
-"⭐": 1,
-"⭐1/2": 1.5,
-"⭐⭐": 2,
-"⭐⭐1/2":2.5,
-"⭐⭐⭐": 3,
-"⭐⭐⭐1/2": 3.5,
-"⭐⭐⭐⭐": 4,
-"⭐⭐⭐⭐1/2": 4.5,
-"⭐⭐⭐⭐⭐": 5,
-"⭐⭐⭐⭐⭐⭐":6,
+    "1/2": 0.5,
+    "⭐": 1,
+    "⭐1/2": 1.5,
+    "⭐⭐": 2,
+    "⭐⭐1/2": 2.5,
+    "⭐⭐⭐": 3,
+    "⭐⭐⭐1/2": 3.5,
+    "⭐⭐⭐⭐": 4,
+    "⭐⭐⭐⭐1/2": 4.5,
+    "⭐⭐⭐⭐⭐": 5,
+    "⭐⭐⭐⭐⭐⭐": 6,
 }
 
-def star_plot(df, output_f):
-  df_mod = df.replace(star_vals)
-  ratings = np.linspace(0.5, 6, 12)
-  fig = go.Figure()
-  fig.add_trace(go.Bar(x=ratings, y=list(df['Bryce Rating'].value_counts().reindex(ratings).replace(np.nan, 0)), name='Bryce', marker_color='indianred'))
-  fig.add_trace(go.Bar(x=ratings, y=list(df['Kathy Rating'].value_counts().reindex(ratings).replace(np.nan, 0)), name='Kathy', marker_color='lightsalmon'))
-  fig.update_xaxes(tickvals=ratings)
-  #fig.add_trace(go.Histogram(df, x='Bryce Rating'))
-  #fig.add_trace(go.Histogram(df, x='Kathy Rating'))
-  #fig.update_layout(barmode='overlay')
-  #fig.update_traces(opacity=0.74) 
-  # https://plotly.com/python-api-reference/generated/plotly.io.to_html.html
-  output_f.write(fig.to_html(full_html=True, include_plotlyjs='cdn'))
-  return fig
+
+def star_plot(df):
+    df_mod = df.replace(star_vals)
+    ratings = np.linspace(0.5, 6, 12)
+    bryce_ratings = df_mod['Bryce Rating'].value_counts().reindex(ratings).replace(np.nan, 0)
+    kathy_ratings = df_mod['Kathy Rating'].value_counts().reindex(ratings).replace(np.nan, 0)
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=ratings, y=list(bryce_ratings), name='Bryce', marker_color='indianred'))
+    fig.add_trace(go.Bar(x=ratings, y=list(kathy_ratings), name='Kathy', marker_color='lightsalmon'))
+    fig.update_xaxes(tickvals=ratings)
+    #fig.add_trace(go.Histogram(df, x='Bryce Rating'))
+    #fig.add_trace(go.Histogram(df, x='Kathy Rating'))
+    #fig.update_layout(barmode='overlay')
+    #fig.update_traces(opacity=0.74) 
+    # https://plotly.com/python-api-reference/generated/plotly.io.to_html.html
+    return fig.to_html(full_html=False, include_plotlyjs='cdn')
+
 
 def normalize_location_str(str_loc):
-  if '::' in str_loc:
-    str_loc = str_loc.split('::')[0]
-  if ' USA' in str_loc:
-    str_loc = str_loc.replace(' USA', '')
-  if ' - ' in str_loc:
-    str_loc = str_loc.split(' - ')[1]
-  return str_loc
+    if str_loc == 'USA':
+        return None
+    if '::' in str_loc:
+        str_loc = str_loc.split('::')[0]
+    if ' USA' in str_loc:
+        str_loc = str_loc.replace(' USA', '')
+    if ' - ' in str_loc:
+        str_loc = str_loc.split(' - ')[1]
+    return str_loc
 
-def my_geocode(query):
-  if query not in cache:
-    cache[query] = query_geocoder_server(query)
-  return cache[query]
-
-cache_path = 'cache.p'
-
-cache = dict()
-if Path(cache_path).exists():
-  cache = pickle.load(open(cache_path, 'rb'))
-
-def query_geocoder_server(query):
-  time.sleep(5)
-  return geolocator.geocode(query)
 
 geolocator = geopy.Nominatim(user_agent='movies_watched_2020')
+def query_geocoder_server(query):
+    time.sleep(5)
+    return geolocator.geocode(query)
+
+
+geo_cache_f = Path(__file__).parent.resolve().joinpath('cache.p')
+geo_cache = pickle.load(geo_cache_f.open('rb')) if geo_cache_f.exists() else dict()
+def my_geocode(query):
+    if query is None:
+        return None
+    if query not in geo_cache:
+        geo_cache[query] = query_geocoder_server(query)
+    return geo_cache[query]
+
 
 def get_locs(df, full_movies):
-  d = {'Movie Name': [], 'lat': [], 'lon': []}
-  for row in df.itertuples():
-    name = row._1
-    print(name)
-    all_locs = full_movies[row.index].get('locations')
-    if all_locs is None:
-      continue
-    for str_loc in full_movies[row.index].get('locations'):
-      str_loc = normalize_location_str(str_loc)
-      loc = my_geocode(str_loc)
-      if loc is None:
-        print('\tSkipping ' + str_loc)
-        continue
-      else:
-        print('Using ' + str_loc)
-      d['Movie Name'].append(name)
-      d['lat'].append(loc.latitude)
-      d['lon'].append(loc.longitude)
-      # break # For now: try again later, once we have persistant caching in place for geopy
-  return pd.DataFrame(d)
-
-def filming_locations(df, full_movies, out_f):
-  loc_df = get_locs(df, full_movies) 
-  pickle.dump(cache, open(cache_path, 'wb'))
-  loc_df['size'] = 12
-  fig = px.line_mapbox(loc_df, lat='lat', lon='lon', color='Movie Name') # , size='size')
-  fig.update_layout(mapbox_style='stamen-terrain')
-  out_f.write(fig.to_html(full_html=True, include_plotlyjs='cdn'))
-  return fig
+    d = {'Movie Name': [], 'lat': [], 'lon': []}
+    for row in df.itertuples():
+        name = row._1
+        all_locs = full_movies[row.index].get('locations')
+        if all_locs is None:
+            continue
+        for str_loc in full_movies[row.index].get('locations'):
+            loc = my_geocode(normalize_location_str(str_loc))
+            if loc is None:
+                continue
+            d['Movie Name'].append(name)
+            d['lat'].append(loc.latitude)
+            d['lon'].append(loc.longitude)
+    return pd.DataFrame(d)
 
 
-def genres_plot(df, full_movies, out_f):
-  # TODO(brycew): improve this viz
-  genre_list = full_movies.apply(lambda x: x.get('genres')).explode()
-  fig = px.bar(genre_list.value_counts())
-  out_f.write(fig.to_html(full_html=True, include_plotlyjs='cdn'))
-  return fig
+def filming_locations(df, full_movies):
+    loc_df = get_locs(df, full_movies)
+    pickle.dump(geo_cache, geo_cache_f.open('wb'))
+    loc_df['size'] = 12
+    all_figs = []
+    for name in loc_df['Movie Name'].unique():
+        temp_df = loc_df[loc_df['Movie Name'] == name]
+        fig = px.scatter_mapbox(temp_df,
+                                lat='lat', lon='lon', size='size',
+                                color='Movie Name', color_discrete_sequence=px.colors.qualitative.G10 * 10)
+        fig.update_layout(mapbox_style='stamen-terrain')
+        all_figs.append( (name, fig.to_html(full_html=False, include_plotlyjs='cdn')) )
+    return all_figs
 
-def budgets_violin_plot(df, full_movies, out_f):
-  locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
-  def grab_budget(dx):
-    if 'box office' not in dx.data or 'Budget' not in dx.data['box office']:
-      return np.nan
-    else:
-      val = dx.data['box office']['Budget'].replace(' (estimated)', '')
-      mult = 1.22 if val.startswith('EUR') else 1
-      return locale.atof(val.strip('$').strip('EUR')) * mult
 
-  buds = full_movies.apply(grab_budget).dropna()
-  df2 = pd.DataFrame()
-  df2['budget'] = buds
-  df2['name'] = df['Movie Name']
-  df2 = df2.sort_values('budget')
-  fig = px.violin(df2, y='budget', box=True, points='all', hover_data=df2.columns)
-  out_f.write(fig.to_html(full_html=True, include_plotlyjs='cdn'))
-  return fig
-  
+def genres_plot(df, full_movies):
+    genre_list = full_movies.apply(lambda x: x.get('genres')).explode()
+    fig = px.bar(genre_list.value_counts())
+    fig.update_layout(width=2000, height=1000) 
+    return fig.to_html(full_html=False, include_plotlyjs='cdn')
+
+
+def budgets_violin_plot(df, full_movies):
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
+    def grab_budget(dx):
+        if 'box office' not in dx.data or 'Budget' not in dx.data['box office']:
+            return np.nan
+        else:
+            val = dx.data['box office']['Budget'].replace(' (estimated)', '')
+            mult = 1.22 if val.startswith('EUR') else 1
+            return locale.atof(val.strip('$').strip('EUR')) * mult
+
+    buds = full_movies.apply(grab_budget).dropna()
+    df2 = pd.DataFrame()
+    df2['budget'] = buds
+    df2['name'] = df['Movie Name']
+    df2['year'] = df['Year']
+    df2 = df2.sort_values('budget')
+    fig = px.violin(df2, y='budget', box=True, points='all', 
+                    hover_data=df2.columns, title='Budgets')
+    fig.update_layout(width=1000, height=1000) 
+    return fig.to_html(full_html=False, include_plotlyjs='cdn')
+
 
 def gather_cast_crew(full_movies):
-  for artist_type in ['directors', 'cast']:
-    artists = collections.defaultdict(int)
-    for mov in full_movies:
-      print(mov.data['original title'])
-      if artist_type in mov.data:
-        for name in mov.data[artist_type]:
-          artists[name] += 1
-    G = nx.Graph()
-    for d_count in artists.items():
-      if d_count[1] > 1:
-        G.add_node(d_count[0], type=artist_type, count=d_count[1])
-    for mov in full_movies:
-      if artist_type in mov.data:
-        all_to_add = []
-        for name in mov.data[artist_type]:
-          if any([node for node in G.nodes(data=True) if node[0].personID == name.personID]):
-            all_to_add.append(name)
-        G.add_edges_from(itertools.combinations(all_to_add, 2))
-  #nx.draw_spring(G, with_labels=True)
-  return G 
+    for artist_type in ['directors', 'cast']:
+        artists = collections.defaultdict(int)
+        for mov in full_movies:
+            print(mov.data['original title'])
+            if artist_type in mov.data:
+                for name in mov.data[artist_type]:
+                    artists[name] += 1
+        G = nx.Graph()
+        for d_count in artists.items():
+            if d_count[1] > 1:
+                G.add_node(d_count[0], type=artist_type, count=d_count[1])
+        for mov in full_movies:
+            if artist_type in mov.data:
+                all_to_add = []
+                for name in mov.data[artist_type]:
+                    if any([node for node in G.nodes(data=True) if node[0].personID == name.personID]):
+                        all_to_add.append(name)
+                G.add_edges_from(itertools.combinations(all_to_add, 2))
+    #nx.draw_spring(G, with_labels=True)
+    return G 
 
 
 # TODO(brycew): at least 3 more viz:
 # * graph of actors / BTS, movies are edges
 #   * https://plotly.com/python/network-graphs/
 # * number of women/PoC BTS and actors
-# * budgets of the movies (just on a density/violin 1D plot) (movie.data['box office'][1]), strip estimated
 
 
 def main(argv):
-  df, full_movies = get_movie_info("../data/GoodMovies2020.csv")
-  with open('plot1.html', 'w') as out_f1:
-    combo_timeline(df, out_f1)
-  with open('plot2.html', 'w') as out_f2:
-    star_plot(df, out_f2)
-  with open('plot3.html', 'w') as out_f3:
-    filming_locations(df, full_movies, out_f3)
+    df, full_movies = get_movie_info("./data/GoodMovies2020.csv")
+    combo_timeline_graph = combo_timeline(df)
+    star_plot_bar_graph = star_plot(df)
+    filming_locations_graph = filming_locations(df, full_movies)
+    genre_plot = genres_plot(df, full_movies)
+    budgets_graph = budgets_violin_plot(df, full_movies)
+
+    mytemplate = Template(filename="./view/webpage.html")
+    with open('./view/webpage_usable.html', 'w') as f:
+      f.write(mytemplate.render(combo_timeline_graph=combo_timeline_graph,
+                                star_plot_bar_graph=star_plot_bar_graph,
+                                filming_locations_graph=filming_locations_graph, 
+                                genre_plot=genre_plot,
+                                budgets_graph=budgets_graph))
 
 
 if __name__ == '__main__':
-  main(sys.argv)
+    main(sys.argv)
